6d57a3cfb9689f5a76246903a2f703b8c97be5e8
Bamieh <ahmadbamieh@gmail.com>
2015-08-05 12:36:28 +0300

    stable version

--- public/angular-bamieh-gcm/bamieh-gcm.js
+++
@@ -1,9 +0,0 @@
-angular
-  .module('angular-bamieh-gcm', [])
-  .provider('bamiehGcmState',bamiehGcmState)
-  .provider('bamiehGcmEndpoint',bamiehGcmEndpoint)
-  .provider('setupPermissions',setupPermissions)
-  .provider('initilization',initilization)
-  .config(registerWorker)
-  .service('subscribeDeviceService', subscribeDeviceService)
-  .controller('appController', appController)
--- public/angular-bamieh-gcm/registerWorker.config.js
+++
@@ -1,9 +0,0 @@
-registerWorker.$inject = ['initilizationProvider'];
-function registerWorker(initilizationProvider) {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('service-worker.js', {scope: '/'})
-      .then(initilizationProvider.initialiseState);
-    } else {
-      console.log('Ooops Service Workers are not Supported.');
-    }
-}
